b5250804ffbb7067bdc191e291fb28a8aff93f77
Your Name <you@example.com>
2020-09-07 12:41:10 -0300

    fix

--- core/signals.py
+++ core/signals.py
@@ -5,7 +5,6 @@ assofrom django.db.models.signals import post_save
 
 @receiver(post_save, sender=Associação)
 def set_is_sócio(sender, instance, **kwargs):
-    print('ok')
     if not instance.sócio.is_sócio:
         instance.sócio.is_sócio = instance.is_active
         instance.sócio.save()
