79d6e4b4089eebfa2c4ab1b14087ca6df5530044
Your Name <you@example.com>
2020-09-11 18:56:52 -0300

    payment serializer comprovante

--- ecommerce/serializers.py
+++ ecommerce/serializers.py
@@ -4,31 +4,37 @@ from .models import Item, OrderItem, Order, Payment
 from core.serializers import UserSerializer
 
 
-
 class OrderItemSerializer(serializers.ModelSerializer):
     item = serializers.StringRelatedField()
     size = serializers.StringRelatedField()
+
     class Meta:
         model = OrderItem
         fields = ['pk', 'item', 'quantity', 'final_price', 'size']
 
+
 class PaymentSerializer(serializers.ModelSerializer):
     gateway = serializers.StringRelatedField(source='get_gateway_display')
+
     class Meta:
         model = Payment
-        fields = ['gateway']
-        
+        fields = ['gateway', 'comprovante']
+
+
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
     status = serializers.StringRelatedField(source='get_status_display')
     payment = PaymentSerializer(read_only=True)
     user = UserSerializer(read_only=True)
+
     class Meta:
         model = Order
-        fields = ['pk', 'user', 'items', 'status', 'ordered_date', 'order_total', 'payment']
+        fields = ['pk', 'user', 'items', 'status',
+                  'ordered_date', 'order_total', 'payment']
 
 
 class ItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ['pk', 'title', 'price', 'socio_price', 'has_variations', 'image']
+        fields = ['pk', 'title', 'price',
+                  'socio_price', 'has_variations', 'image']
